e21cf72dabf260886025c2a9d0961f838af276ab
haya1007 <hayato.cha20021007@gmail.com>
2022-02-26 18:42:32 +0900

    アイテム名が変わるように

--- ThunderSword/src/ThunderSword/Main.php
+++ ThunderSword/src/ThunderSword/Main.php
@@ -9,7 +9,6 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\item\ItemFactory;
 use pocketmine\utils\Config;
 
-use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\IntTag;
 
 class Main extends PluginBase{
@@ -21,25 +20,23 @@ class Main extends PluginBase{
 		$this->saveDefaultConfig();
 		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 
-		$this->sword = ItemFactory::getInstance()->get(283, 0);
+		// 雷の剣
+		$this->sword = ItemFactory::getInstance()->get(283, 0); // 金の剣の取得
+		$this->sword->setCustomName($this->config->get('NAME')); // 名前の変更
+		$this->sword->setLore([$this->config->get('LORE')]); // アイテムの説明欄
 		if($this->config->get("Unbreakable")){
-   			$this->sword->setUnbreakable();
+   			$this->sword->setUnbreakable(); // 壊れないようにする
    		}
-		$this->sword->setCustomName($this->config->get('NAME'));
-		$this->sword->setLore(["雷を落とす剣"]);
-		$tag = new CompoundTag();
-		$tag->setTag("thunder", new IntTag(1));
-		$tag->setTag("atk", new IntTag($this->config->get("ATK")));
-		$this->sword->setNamedTag($tag);
+		$tag = $this->sword->getNamedTag(); // nbt取得
+		$tag->setTag("thunder", new IntTag(1)); // 雷の剣なのかの判定用
+		$tag->setTag("atk", new IntTag($this->config->get("ATK"))); // ダメージ量
+		$this->sword->setNamedTag($tag); // nbtの適用
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch($command->getName()){
 			case "sword":
 				$sender->getInventory()->addItem($this->sword);
-				$thunder = $this->sword->getNamedTag()->getTag("thunder");
-				$atk = $this->sword->getNamedTag()->getTag("atk");
-				$sender->sendMessage("thunder: ".$thunder."\natk: ".$atk."");
 
 				return true;
 		}
